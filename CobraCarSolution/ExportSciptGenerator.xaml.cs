28ecf49ba0e751e1125d4d34063ca27216b24d03
admin <admin@DESKTOP-9V3V2HQ>
2023-04-20 20:05:51 +0200

    start comparer

--- CobraCarSolution/ExportSciptGenerator.xaml.cs
+++ CobraCarSolution/ExportSciptGenerator.xaml.cs
@@ -3,10 +3,12 @@
 using CobraCarSolution.TreeElements.VAG;
 
 using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.IO;
 using System.Reflection;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -22,10 +24,7 @@ namespace CobraCarSolution
     /// </summary>
     public partial class ExportSciptGenerator : Window
     {
-        public bool egrOffEffected = false;
-        public bool dpfOffEffected = false;
-        public bool dtcOffEffected = false;
-
+        public string orgFilePath = "";
 
         public ExportSciptGenerator()
         {
@@ -36,6 +35,31 @@ namespace CobraCarSolution
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
 
+        }
+        private bool isPathSet(string pathname)
+        {
+            if (!string.IsNullOrEmpty(pathname) && !pathname.StartsWith("file not"))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (isPathSet(orgFilePath))
+            {
+                List<DiffCollection> diffCollectionFromUI = new List<DiffCollection>();
+                byte[] org = File.ReadAllBytes(egrPath.Text);
+                if (isPathSet(egrPath.Text))
+                {
+                    DiffCollection egrCollection = new DiffCollection();
+                    egrCollection.solutionName = "egrSolution";
+                    egrCollection.compare = File.ReadAllBytes(egrPath.Text);
+                }
+            }
         }
     }
 }
